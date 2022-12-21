be5dbcd34454d436dda27e7bad3e1910a9064c5b
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-21 16:36:55 +0100

    boucle do while 2

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,20 @@
 // BOUCLE 3.1
 
-let arr = ['ab','abc','abcd','abcde'];
-for (let index = 0; index < arr.length; index++) {
-    if (arr[index].length>=4) {
-        console.log(arr[index]);
+// let arr = ['ab','abc','abcd','abcde'];
+// for (let index = 0; index < arr.length; index++) {
+//     if (arr[index].length>=4) {
+//         console.log(arr[index]);
+//     }
+// }
+
+// BOUCLE 3.2
+
+let arr = ['voiture','bus','moto','avion']
+console.log(arr);
+let saisie = prompt("Saisie un élément du tableau");
+do {
+    if (saisie.includes('a')) {
+        alert("Bien joué");
     }
-}
+    saisie = prompt("Saisie un élément du tableau");
+} while (!arr.includes('a'));

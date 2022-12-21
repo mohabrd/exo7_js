a743275ecb0c89e29e95dda354215fd9d635cfa0
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-21 15:21:07 +0100

    boucle for 1

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+// BOUCLE 3.1
+
+let arr = ['ab','abc','abcd','abcde'];
+for (let index = 0; index < arr.length; index++) {
+    if (arr[index].length>=4) {
+        console.log(arr[index]);
+    }
+}
